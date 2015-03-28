6cafa49a2430506e7034ddeac0d1a62817be8989
NicholasMarengo <marengon13@gmail.com>
2015-03-28 14:41:22 -0400

    Lab 7 progress
    I understand that this is  SQL stuff and not a PDF but I just wanted to
    push my progress so far. My completed lab will be a PDF, I pinky swear.
    
    Table in First Normal Form

---
+++ lab7.sql
@@ -0,0 +1,27 @@
+﻿--1--
+--Nick, focus on the SQL and table first. You got it other Nick.--
+
+
+--2-- --first normal form (firstnf)--
+
+DELETE FROM firstnf
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('AC01', 32808, '09-13-2005', 754.95);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('DB32', 32808, '12-03-2005', 380.00);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('DB32', 37691, '06-15-2005', 380.00);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('DB33', 57772, '05-27-2005', 412.77);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP08', 32808, '01-12-2006', 185.00);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP08', 37691, '06-15-2005', 227.50);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP08', 57222, '05-27-2005', 170.24);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP09', 59836, '10-30-2005', 35.00);
+
+INSERT INTO firstnf (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP09', 77740, '05-27-2005', 35.00);
+
+
