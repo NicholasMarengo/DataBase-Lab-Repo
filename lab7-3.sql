292cc0bf70e74d6226655ed50b2c4fc45d5edb17
NicholasMarengo <marengon13@gmail.com>
2015-03-29 21:54:12 -0400

    lab 7 progress
    A somewhat close to completed lab 7

--- lab7-3.sql
+++ lab7-3.sql
@@ -30,7 +30,27 @@ INSERT INTO thirdnft2 (tagnumber, computermodel) VALUES (59836, 'Acer');
 
 INSERT INTO thirdnft2 (tagnumber, computermodel) VALUES (77740, 'Sony');
 
---thirdnft3--
+--thirdnft3-- --same as the first table--
 
 DELETE FROM thirdnft3
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('AC01', 32808, '09-13-2005', 754.95);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('DB32', 32808, '12-03-2005', 380.00);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('DB32', 37691, '06-15-2005', 380.00);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('DB33', 57772, '05-27-2005', 412.77);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP08', 32808, '01-12-2006', 185.00);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP08', 37691, '06-15-2005', 227.50);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP08', 57222, '05-27-2005', 170.24);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP09', 59836, '10-30-2005', 35.00);
+
+INSERT INTO thirdnft3 (packageid, tagnumber, installdate, softwarecostusd) VALUES ('WP09', 77740, '05-27-2005', 35.00);
+
+
 
